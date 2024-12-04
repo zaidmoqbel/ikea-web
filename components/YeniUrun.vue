59260a3bdd11866eb4341d30523a159abf23fd4d
zaidmoqbel <zaidmoqbel01@gmail.com>
2024-12-04 13:15:14 +0300

    Initial commit

--- components/YeniUrun.vue
+++ components/YeniUrun.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="text-container" align="left" style="margin: 60px 98px 30px 98px">
+    <div class="text-container" align="left" style="margin: 60px 98px 30px 0px">
         <h2 style="padding-bottom:20px ; ">Yeni ürünlerimiz burada!</h2>
         <p style="padding-bottom:15px ;font-size: 0.85rem;">Eviniz için yeni ürünlerimizi keşfedin. Mobilya ve aksesuar ürünleri
                                                             arasından seçim yapabileceğiniz yeni ürünlerimizle evinizi
@@ -9,38 +9,38 @@
 
     </div>
 
-    <div class="image-container" style="margin-left: 85px; ">
+    <div class="image-container">
         <v-row>
             <v-col cols="3">
                 <div class="image-parent">
-                    <img src="../assets/yeni-urun-imgs/yeni.png" alt="" style="width: 270px; height: 270px;">
+                    <img src="../assets/yeni-urun-imgs/yeni.png" alt="" style="width:280px; height: 270px;">
                 </div>
                 <div class="image-parent">
-                    <img src="../assets/yeni-urun-imgs/vinterfin-mantar-agac-susu.png" alt="" style="width: 270px; height: 365px;">
-                </div>
-            </v-col>
-            <v-col cols="3">
-                <div class="image-parent">
-                    <img src="../assets/yeni-urun-imgs/vinterfint-hediye-kesesi.png" alt="" style="width: 270px; height: 365px;">
-                </div>
-                <div class="image-parent">
-                    <img src="../assets/yeni-urun-imgs/vinterfint-noel-baba-desenli-pecete.png" alt=""  style="width: 270px; height: 270px;">
+                    <img src="../assets/yeni-urun-imgs/vinterfin-mantar-agac-susu.png" alt="" style="width:280px; height: 365px;">
                 </div>
             </v-col>
             <v-col cols="3">
                 <div class="image-parent">
-                    <img src="../assets/yeni-urun-imgs/vinterfint-noel-baba-desenli-paspas.png" alt=""  style="width: 270px; height: 270px;">
+                    <img src="../assets/yeni-urun-imgs/vinterfint-hediye-kesesi.png" alt="" style="width:280px; height: 365px;">
                 </div>
                 <div class="image-parent">
-                    <img src="../assets/yeni-urun-imgs/vinterfint-hediye-paketi-kagidi.png" alt="" style="width: 270px; height: 365px;">
+                    <img src="../assets/yeni-urun-imgs/vinterfint-noel-baba-desenli-pecete.png" alt=""  style="width:280px; height: 270px;">
                 </div>
             </v-col>
             <v-col cols="3">
                 <div class="image-parent">
-                    <img src="../assets/yeni-urun-imgs/vinterfint-altın-agac-susu.png" alt="" style="width: 270px; height: 365px;">
+                    <img src="../assets/yeni-urun-imgs/vinterfint-noel-baba-desenli-paspas.png" alt=""  style="width:280px; height: 270px;">
                 </div>
                 <div class="image-parent">
-                    <img src="../assets/yeni-urun-imgs/vinterfint-bardakta-kokulu-mum.png" alt=""  style="width: 270px; height: 270px;">
+                    <img src="../assets/yeni-urun-imgs/vinterfint-hediye-paketi-kagidi.png" alt="" style="width:280px; height: 365px;">
+                </div>
+            </v-col>
+            <v-col cols="3">
+                <div class="image-parent">
+                    <img src="../assets/yeni-urun-imgs/vinterfint-altın-agac-susu.png" alt="" style="width:280px; height: 365px;">
+                </div>
+                <div class="image-parent">
+                    <img src="../assets/yeni-urun-imgs/vinterfint-bardakta-kokulu-mum.png" alt=""  style="width:280px; height: 270px;">
                 </div>
             </v-col>
         </v-row>
