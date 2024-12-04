33588482105b4ab02ee9fa7379317e68c6244da5
zaidmoqbel <zaidmoqbel01@gmail.com>
2024-12-04 12:50:20 +0300

    Initial commit

--- pages/sign-up.vue
+++ pages/sign-up.vue
@@ -34,7 +34,6 @@
                         class="email-box"
                     ></v-textarea>
 
-
               <v-btn rounded size="large" style="width: 384.656px;height: 54px; margin-top: 40px;" class="text-none " color="#0058a3" dark>Devam Et</v-btn>
               <div class="yardim">Yardıma İhtiyacım Var</div>
             </div>
